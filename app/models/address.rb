b1c32eccdd3eb5cf6ecfb03b7b94fd2bbc11940c
Tyler Rick <tyler@k3integrations.com>
2011-08-17 21:20:41 -0700

    Added Address#identical?

--- app/models/address.rb
+++ app/models/address.rb
@@ -65,5 +65,14 @@ class Address < ActiveRecord::Base
       out << country_name if country.present?
     end
   end
+
+  def self.attributes_to_ignore_when_comparing
+    [:id, :created_at, :updated_at]
+  end
+
+  def identical?(other)
+    self. attributes.except(*self.class.attributes_to_ignore_when_comparing.map(&:to_s)) ==
+    other.attributes.except(*self.class.attributes_to_ignore_when_comparing.map(&:to_s))
+  end
   
 end
